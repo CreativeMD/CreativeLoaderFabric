dbfe2e9ee7522ef32551ae59d8d9d85ee8d0a3d9
CreativeMD <Creative-Multimedia-Design@gmx.de>
2022-07-25 12:46:14 +0200

    Adapted to CreativeCore changes

--- src/main/java/team/creative/creativecore/CreativeCore.java
+++ src/main/java/team/creative/creativecore/CreativeCore.java
@@ -36,10 +36,10 @@ import team.creative.creativecore.common.config.holder.CreativeConfigRegistry;
 import team.creative.creativecore.common.config.sync.ConfigurationChangePacket;
 import team.creative.creativecore.common.config.sync.ConfigurationClientPacket;
 import team.creative.creativecore.common.config.sync.ConfigurationPacket;
+import team.creative.creativecore.common.gui.creator.GuiCreator;
+import team.creative.creativecore.common.gui.creator.GuiCreator.GuiCreatorBasic;
+import team.creative.creativecore.common.gui.creator.GuiLayerCreator;
 import team.creative.creativecore.common.gui.dialog.GuiDialogHandler;
-import team.creative.creativecore.common.gui.handler.GuiCreator;
-import team.creative.creativecore.common.gui.handler.GuiCreator.GuiCreatorBasic;
-import team.creative.creativecore.common.gui.handler.GuiLayerHandler;
 import team.creative.creativecore.common.gui.integration.ContainerIntegration;
 import team.creative.creativecore.common.gui.packet.ControlSyncPacket;
 import team.creative.creativecore.common.gui.packet.LayerClosePacket;
@@ -106,9 +106,9 @@ public class CreativeCore implements ModInitializer {
         
         ArgumentTypeInfosAccessor.getByClass().put(StringArrayArgumentType.class, SingletonArgumentInfo.contextFree(() -> StringArrayArgumentType.stringArray()));
         
-        GuiLayerHandler.REGISTRY.register("info", GuiInfoStackButton.INFO_LAYER);
-        GuiLayerHandler.REGISTRY.register("player", GuiPlayerSelectorButton.PLAYER_LAYER);
-        GuiLayerHandler.REGISTRY.register("dialog", GuiDialogHandler.DIALOG_HANDLER);
+        GuiLayerCreator.REGISTRY.register("info", GuiInfoStackButton.INFO_LAYER);
+        GuiLayerCreator.REGISTRY.register("player", GuiPlayerSelectorButton.PLAYER_LAYER);
+        GuiLayerCreator.REGISTRY.register("dialog", GuiDialogHandler.DIALOG_HANDLER);
     }
     
     public static ICreativeLoader loader() {
